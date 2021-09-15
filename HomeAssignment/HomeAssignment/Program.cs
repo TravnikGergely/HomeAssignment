cfd21737f89fa2ceca2442339a48bfde573484c0
Travnik Gergely <travnik.gergely@gmail.com>
2021-09-15 14:09:46 +0200

    The program waits for an enter before stopping.

--- HomeAssignment/HomeAssignment/Program.cs
+++ HomeAssignment/HomeAssignment/Program.cs
@@ -9,6 +9,7 @@
             Console.WriteLine("Please enter 0 to 4 DIGITS between 2 and 9!");
             string input = Console.ReadLine();
             Console.WriteLine(Solution.Combinations(input));
+            Console.ReadKey();
         }
     }
 }
